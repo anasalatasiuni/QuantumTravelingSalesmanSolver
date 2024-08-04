a005c4294a08ac194e12da74a157adb9f3670221
Murhaf Alawir <94635412+thecarrot123@users.noreply.github.com>
2024-08-04 15:06:33 +0300

    run sampler multiple times

--- code/anas_murhaf_solver.py
+++ code/anas_murhaf_solver.py
@@ -1,11 +1,9 @@
 import sys
 import time
-from dwave.embedding.chain_strength import scaled
 from dwave.system.composites import EmbeddingComposite
-from dwave.system.samplers import DWaveSampler
+from dwave.system import DWaveSampler
 import dwave.inspector
-import matplotlib.pyplot as plt
-import networkx as nx
+
 import numpy as np
 from plot import plot_solution
 
@@ -68,17 +66,15 @@ Q = build_objective_matrix()
 np.set_printoptions(threshold=sys.maxsize)
 with open("Q.txt", 'w') as f:
     f.write(f"{Q}\n")
-    
-lagrange_multiplier = np.max(np.abs(M))
 
 def is_valid_solution(X):
-    rows, cols = X.shape
-    for i in range(rows):
+    n, _ = X.shape
+    for i in range(n):
         count = 0
-        for j in range(cols):
+        for j in range(n):
             if X[i,j] == 1:
                 count += 1
-        if not count == 2:
+        if count != 1:
             return False
     return True
     
@@ -118,47 +114,42 @@ def score(M, X):
     return ans, path
 
 
-n, _ = M.shape
+def solve(best_energy):
+    n, _ = M.shape
+    sampler = EmbeddingComposite(DWaveSampler())
+    t0 = time.perf_counter()
+    sampleset = sampler.sample_qubo(Q, num_reads=num_samples)
+    t1 = time.perf_counter()
+    dwave.inspector.show(sampleset)
+    problem_id = sampleset.info['problem_id']
 
-sampler = EmbeddingComposite(DWaveSampler()) # QPU sampler to run in production
-t0 = time.perf_counter()
-sampleset = sampler.sample_qubo(Q, num_reads=num_samples)
-t1 = time.perf_counter()
-dwave.inspector.show(sampleset)
-have_solution = False
-problem_id = sampleset.info['problem_id']
-chain_strength = sampleset.info['embedding_context']['chain_strength']
-
-with open(out_file, 'w') as f:
-    f.write(f"Problem Id: {problem_id}\n")        # does not depend on sample  
-    count = 0
     for e in sampleset.data(sorted_by='energy'):
         sample = e.sample
         energy = e.energy
         num_occurrences = e.num_occurrences
         chain_break_fraction = e.chain_break_fraction
-        X = build_solution(sample)
-        # if is_valid_solution(X):
-        have_solution = True
-        score, path = score(M, X)
-        f.write(f"Solution:\n")
-        f.write(f"{X}\n")
-        f.write(f"Score: {score}\n")
-        f.write(f"{sample}\n")
-        f.write(f"index: {count}\n")
-        f.write(f"energy: {energy}\n")
-        f.write(f"num_occurrences: {num_occurrences}\n")            
-        f.write(f"chain break fraction: {chain_break_fraction}\n")            
-        break   # break out of for loop
-        count += 1
-    f.write(f"chain strength: {chain_strength}\n")  # does not depend on sample
-    f.write(f"lagrange multiplier: {lagrange_multiplier}\n")
-    f.write(f"Time: {t1-t0:0.4f} s\n")
-    if not have_solution:
-        # https://docs.ocean.dwavesys.com/en/latest/examples/inspector_graph_partitioning.html
-        # this is the overall chain break fraction
-        chain_break_fraction = np.sum(sampleset.record.chain_break_fraction)/num_samples
-        f.write("did not find any solution\n")
-        f.write(f"chain break fraction: {chain_break_fraction}\n")
         
-plot_solution( n, path , M)
+        if best_energy <= energy:
+            return best_energy
+
+        with open(out_file, 'w') as f:
+            X = build_solution(sample)
+            if is_valid_solution(X):
+                f.write(f"Problem Id: {problem_id}\n")
+                best_energy = energy
+                cost, path = score(M, X)
+                f.write(f"Solution:\n")
+                f.write(f"{X}\n")
+                f.write(f"Score: {cost}\n")
+                f.write(f"{sample}\n")
+                f.write(f"energy: {energy}\n")
+                f.write(f"num_occurrences: {num_occurrences}\n")            
+                f.write(f"chain break fraction: {chain_break_fraction}\n")            
+                f.write(f"Time: {t1-t0:0.4f} s\n")
+                break
+            
+    return best_energy
+
+best_energy = 1e9
+for _ in range(4):
+    best_energy = solve(best_energy)
